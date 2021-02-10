639d8ddb943297830668398f412debd35551438e
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2021-02-10 01:43:27 -0500

    adding stuff

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
