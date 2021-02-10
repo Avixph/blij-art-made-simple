19e1d77ecbefb69bfc80b89b7ae8685e60eb750c
Jennifer Smith <59855990+JenniferSmith007@users.noreply.github.com>
2021-02-10 18:59:24 -0500

    Angel and I, created reducers for redux set up proper folder structer and are starting to form our actions and moved router to app

---
+++ src/reducer/deptReducers.js
@@ -0,0 +1,21 @@
+import * as actions from "../actions/deptActions";
+
+export const initialState = {
+  artDept: [],
+  loading: false,
+  pageError: false,
+};
+
+export default function artDeptReducers(state = initialState, action) {
+  switch (action.type) {
+    case actions.Get_ArtDept:
+      return { ...state, loading: true };
+    case actions.Get_ArtDept_Success:
+      return { artDept: action.payload, loading: false, pageError: false };
+    case actions.Get_ArtDept_Fail:
+      return { ...state, loadig: false, pageError: true };
+
+    default:
+      return state;
+  }
+}
