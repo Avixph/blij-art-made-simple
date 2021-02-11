23728d9392e3281b6cd3ea1cee8c859d41967c27
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2021-02-11 03:07:21 -0500

    Extra testing of the actions

--- src/componentScreens/ArtMain.js
+++ src/componentScreens/ArtMain.js
@@ -1,13 +1,20 @@
-// import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { fetchDepartments } from "../actions/deptActions";
+import { useDispatch } from "react-redux";
+
 export default function ArtMain() {
+  const dispatch = useDispatch();
+  
   return (
     <main className="Art-Main">
       <h1>Angel</h1>
       {/* <NavLink to="/artDepartment" className="bttn">
         View Departments
   </NavLink>} */}
-      <button onClick={fetchDepartments}>yoo</button>
+      <NavLink to="/artDepartments" className="button">
+        yoooooooooooo
+      </NavLink>
+      <button onClick={() => dispatch(fetchDepartments())}>yoo</button>
     </main>
   );
 }
--- src/components/ArtDepartmentResults.js
+++ src/components/ArtDepartmentResults.js
@@ -1,3 +1,11 @@
-export default function ArtDepartmentResults() {
-  return <div></div>;
+function ArtDepartmentResults({ artDepts }) {
+  console.log(artDepts);
+  return (
+    <div className="department-view">
+      <h1>department #:{artDepts.departmentId}</h1>
+      <h2>department name:{artDepts.displayName}</h2>
+    </div>
+  );
 }
+
+export default ArtDepartmentResults;
