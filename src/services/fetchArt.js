d9bf08ef997735de83b5df99fff3d8c0f5cdf876
Jennifer Smith <59855990+JenniferSmith007@users.noreply.github.com>
2021-02-12 05:48:19 -0500

    Search Input works, need to figure out how to dispaly the props being called on

---
+++ src/services/fetchArt.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { MET_URL } from "./constants";
+
+export async function fetchArtByQuery(query) {
+  const searchAPI = `${MET_URL}/public/collection/v1/search?hasImages=true&q=${query}`;
+  const searchInfo = await axios.get(searchAPI).then(({ data }) => {
+    return data;
+  });
+  return searchInfo;
+}
+
+export async function fetchArtByObjectID() {
+  const artAPI = `${MET_URL}/public/collection/v1/objects`;
+  const artInfo = await axios.get(artAPI).then(({ data }) => {
+    return data;
+  });
+  return artInfo;
+}
